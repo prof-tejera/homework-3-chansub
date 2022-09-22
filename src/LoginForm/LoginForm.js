04618da3adbfb800da72aecd1654ab7290bb4ae1
Susana <steymourian@hbs.edu>
2022-09-22 00:15:47 -0400

    Added props
    Added props and onChange behavior to login form.

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,24 +1,46 @@
+import { useState } from "react";
 import "./LoginForm.css";
 import Panel from "components/Panel/Panel";
 import Button from "components/Button/Button";
 import Input from "components/Input/Input";
 
 const LoginForm = () => {
-  
-  const handleClick = (type) => {
-    console.info("type is", type);
+  const [myUsername, setUsername] = useState('');
+  const [activeType, setActiveType] = useState('disabled');
+
+  const handleChange = event => {
+    setUsername(event.target.value);
+
+    let letterCnt  = myUsername.trim().length;
+    
+    //Enable login button only if username has 2 or more characters
+    if(letterCnt > 1){
+      setActiveType('enabled');
+      console.info("onChange enabled");
+    }
+    else if (letterCnt < 2) {
+      setActiveType('disabled');
+      console.info("onChange disabled");
+    }
   };
   
+  const handleClick = event => {
+    event.preventDefault();
+    console.info("Login button pushed!");
+  };
   
-  return (<Panel>
+  return (<Panel className="LoginContainer">
 
-    <Panel myClass="susanaChan"><Input name="username" type="text" maxlength="200" placeholder="Username"/></Panel>
+    <Panel><Input id="username" name="username" type="text" placeholder="Username" onChange={handleChange}/></Panel>
     
-    <Panel><Input name="password" type="password" maxlength="200" placeholder="Password"/></Panel>
+    <Panel><Input id="password" name="password" type="password" placeholder="Password"/></Panel>
 
     <Button
-      onClick={() => handleClick("login")}
+      onClick={handleClick}
       text="Login"
+      id = "loginSubmit"
+      className = {"btnSubmit-" + activeType}
+      disabled= {(activeType === 'disabled') ? true : false}
     />
     </Panel>
   );
--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Panel = (props, myClass) => {
-  return <div className="{$myClass}">{props.children}</div>;
+const Panel = (props) => {
+  return <div className={props.className}>{props.children}</div>;
 };
 
 export default Panel;
